7199b8fd45ba5be7213cc02f8cd90291eb16e91b
Ahmad Ruslan <aruslan8395@gmail.com>
2020-05-23 15:46:16 +0700

    [STYLE]Styling list item

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,56 +9,46 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 440,
-      child: transaction.isEmpty ? Column(
-        children: <Widget>[
-          Text('No Transactions added yet', style: Theme.of(context).textTheme.title,),
-          SizedBox(height: 10),
-          Container(
-            height: 400,
-            child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover)),
-        ],
-      ) :
-      ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-              child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
+      child: transaction.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No Transactions added yet',
+                  style: Theme.of(context).textTheme.title,
                 ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Theme.of(context).primaryColorDark,
-                  width: 2,
-                )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Rp.${transaction[index].amount}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    transaction[index].title,
-                    style: Theme.of(context).textTheme.title
+                SizedBox(height: 10),
+                Container(
+                    height: 400,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover)),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('Rp.${transaction[index].amount}'),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      transaction[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                        DateFormat.yMMMd().format(transaction[index].date)),
+                    trailing: Icon(Icons.delete),
                   ),
-                  Text(DateFormat.yMMMMEEEEd().format(transaction[index].date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 12,
-                      ))
-                ],
-              )
-            ],
-          ));
-        },
-        itemCount: transaction.length,
-      ),
+                );
+              },
+              itemCount: transaction.length,
+            ),
     );
   }
 }
